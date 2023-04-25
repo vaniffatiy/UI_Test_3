6d5a7800fb755e806516de386c1aac2cbbba12ac
vaniffatiy <mytsykov73@mail.ru>
2023-04-25 18:14:16 +0300

    edit2

--- MY_python_pytest/src/locators/drag_page_locators.py
+++ MY_python_pytest/src/locators/drag_page_locators.py
@@ -2,10 +2,10 @@ from selenium.webdriver.common.by import By
 
 
 class DragPageLocators:
-    url = "https://www.globalsqa.com/demo-site/draganddrop/"
-    SOURCE_IMG = '//img[@alt="The peaks of High Tatras"]'
-    TRASH_FIELD = '//div[@id="trash"]'
-    SOURCE_IMG2 = '//img[@alt="The chalet at the Green mountain lake"]'
-    OFFSET = {"xoffset": 100, "yoffest": 0}
+    url = "https://demo.automationtesting.in/Static.html"
+    SOURCE_IMG = '//img[@id="angular"]'
+    TRASH_FIELD = '//div[@id="droparea"]'
+    SOURCE_IMG2 = '//img[@id="mongo"]'
+    OFFSET = {"xoffset": 20, "yoffest": 0}
 
 
--- MY_python_pytest/src/tests/test_drag.py
+++ MY_python_pytest/src/tests/test_drag.py
@@ -1,4 +1,9 @@
+from UI_test.UI_Test_3.MY_python_pytest.src.common.webdriver_handler import WebDriverHandler
 from UI_test.UI_Test_3.MY_python_pytest.src.pages.drag_page import DragPage
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium import webdriver
 import time
 
 
@@ -7,9 +12,9 @@ class TestDynamic:
     def test_drag_drop(self, webdriver_handler):
         drag = DragPage(webdriver_handler)
         drag.open_site()
-        time.sleep(6)
+        time.sleep(15)
         drag.drag_drop()
-        drag.drag_drop_offset()
+        # drag.drag_drop_offset()
 
 
 
